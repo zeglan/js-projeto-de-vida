3f04c8ade33a0d4ccc4f6210767099204d10115b
zeglan <151922548+zeglan@users.noreply.github.com>
2025-05-20 07:38:40 -0300

    ..

---
+++ main.js
@@ -0,0 +1,24 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+
+for (let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+ for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+            botoes[i].classList.add("ativo");
+            textos[i].classList.add("ativo");
+    }
+    }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo (tempoObjetivo){
+   let tempoAtual = new Date();
+   let tempoFinal= tempoObjetivo - tempoAtual;
+   let segundos = Math.floor(tempoFinal)
